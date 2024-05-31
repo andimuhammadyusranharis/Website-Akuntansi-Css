ed90fc90bd3d24552c29bdadb52163c72ae05f25
unknown <andimuhammadyusranharis@gmail.com>
2024-05-31 19:29:06 +0800

    Update Tentang Kami

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
+// Untuk sidebar ketika tampilan responsive
 document.addEventListener('DOMContentLoaded', function() {
-  var navbar = document.getElementById('navbar');
-  var menuBtn = document.getElementById('menu-btn');
-  var sidebar = document.getElementById('sidebar');
-  var hamburgerIcon = document.getElementById('hamburger-icon');
-  var closeIcon = document.getElementById('close-icon');
+    var navbar = document.getElementById('navbar');
+    var menuBtn = document.getElementById('menu-btn');
+    var sidebar = document.getElementById('sidebar');
+    var hamburgerIcon = document.getElementById('hamburger-icon');
+    var closeIcon = document.getElementById('close-icon');
 
-  menuBtn.addEventListener('click', function() {
-      if (sidebar.classList.contains('-translate-x-full')) {
-          sidebar.classList.remove('-translate-x-full');
-          hamburgerIcon.classList.add('hidden');
-          closeIcon.classList.remove('hidden');
-      } else {
-          sidebar.classList.add('-translate-x-full');
-          hamburgerIcon.classList.remove('hidden');
-          closeIcon.classList.add('hidden');
-      }
-  });
+    menuBtn.addEventListener('click', function() {
+        if (sidebar.classList.contains('active')) {
+            sidebar.classList.remove('active');
+            hamburgerIcon.classList.remove('hidden');
+            closeIcon.classList.add('hidden');
+        } else {
+            sidebar.classList.add('active');
+            hamburgerIcon.classList.add('hidden');
+            closeIcon.classList.remove('hidden');
+        }
+    });
 
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 0) {
-          navbar.classList.remove('navbar-transparent');
-          navbar.classList.add('navbar-colored');
-      } else {
-          navbar.classList.remove('navbar-colored');
-          navbar.classList.add('navbar-transparent');
-      }
-  });
+// Untuk mengubah header ketika di scroll
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            navbar.classList.remove('navbar-transparent');
+            navbar.classList.add('navbar-colored');
+        } else {
+            navbar.classList.remove('navbar-colored');
+            navbar.classList.add('navbar-transparent');
+        }
+    });
 });
